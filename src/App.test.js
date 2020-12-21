f2cbb60316da3fc71c4ce3ca5a9b8d2fa5e81093
darkeyz <ovrclockz@gmail.com>
2020-12-21 09:08:11 +0100

    Add: Minimal rendering test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders Grid showing whose turn it is', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const whoseTurn = screen.getByText(/Turn/i);
+  expect(whoseTurn).toBeInTheDocument();
 });
